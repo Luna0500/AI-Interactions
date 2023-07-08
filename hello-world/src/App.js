4cc348b37ef9e8882d5170dbfe4183d6be18ce15
Ryan Lunas <122395549+Luna0500@users.noreply.github.com>
2023-07-08 15:12:55 -0600

    integration

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,19 +1,8 @@
 import './App.css';
 
-async function myFunction() {
-  const users = db.getCollection("users")
-  document.getElementById("myText").innerHTML = await users.find();
-}
 function App() {
   return (
-      <html>
-      <body onLoad="myFunction()">
-        <div>
-          <h1>Hello World!\n</h1>
-          <h2><span id="myText"></span></h2>
-        </div>
-      </body>
-      </html>
+      <h1>Hello World!\n</h1>
   );
 }
 
